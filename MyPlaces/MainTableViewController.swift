38a2bcbeaff86c0f2d3cc2ecbae174432a95e68a
Bolotov Alexandr <projectfebr@gmail.com>
2020-12-01 22:18:12 +0300

    Configure the Cell

--- MyPlaces/MainTableViewController.swift
+++ MyPlaces/MainTableViewController.swift
@@ -9,6 +9,13 @@ import UIKit
 
 class MainTableViewController: UITableViewController {
 
+    let restaurantNames = [
+        "Burger Heroes", "Kitchen", "Bonsai", "Дастархан",
+        "Индокитай", "X.O", "Балкан Гриль", "Sherlock Holmes",
+        "Speak Easy", "Morris Pub", "Вкусные истории",
+        "Классик", "Love&Life", "Шок", "Бочка"
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,25 +28,22 @@ class MainTableViewController: UITableViewController {
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return restaurantNames.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
 
-        // Configure the cell...
+//        cell.textLabel?.text = restaurantNames[indexPath.row]
+        
+        var contentConfiguration = cell.defaultContentConfiguration()
+        contentConfiguration.text = restaurantNames[indexPath.row]
+        cell.contentConfiguration = contentConfiguration
 
         return cell
     }
-    */
 
     /*
     // Override to support conditional editing of the table view.
