7e64adfa47325c698d7fa654966b2a37848da12b
Bolotov Alexandr <projectfebr@gmail.com>
2020-12-02 00:27:50 +0300

    CustomTableViewCell

---
+++ MyPlaces/CustomTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by Александр Болотов on 02.12.2020.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    
+
+}
