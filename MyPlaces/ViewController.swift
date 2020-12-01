ee1fb2cf95f40171c2fa2ad7faa61e481e1fc841
Bolotov Alexandr <projectfebr@gmail.com>
2020-12-01 20:16:20 +0300

    Initial Commit

---
+++ MyPlaces/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyPlaces
+//
+//  Created by Александр Болотов on 01.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
