37970033111526504d25145d93041f89e2e385a9
Bolotov Alexandr <projectfebr@gmail.com>
2020-12-04 02:21:29 +0300

    MapViewController

---
+++ MyPlaces/MapViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MapViewController.swift
+//  MyPlaces
+//
+//  Created by Александр Болотов on 04.12.2020.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func closeVC() {
+        dismiss(animated: true, completion: nil)
+    }
+}
